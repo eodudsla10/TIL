abb25ab5619cfdf2995e4ee61c4dba96a8d43a69
jeondeayeong <eodudsla10@naver.com>
2021-01-06 09:41:11 +0900

    조건문/반복문

--- JS_workspace/js/command.js
+++ JS_workspace/js/command.js
@@ -1,7 +1,7 @@
 //1.조건문
 //if(조건식) 참일때 실행문;
 //if(조건식) {참일때 실행문} else {거짓일때 실행문};
-let month =prompt("월 입력:", "");
+let month =prompt("월 입력:", ""); 
 
 
 if(month ==1 ||month==3 ||month==5||month==7||month==8||month==10||month==12){
