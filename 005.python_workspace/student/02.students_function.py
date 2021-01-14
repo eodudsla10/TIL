4dae1df58928085c5e840b1873c7b5a360a96363
jeondeayeong <eodudsla10@naver.com>
2021-01-14 22:10:28 +0900

    student mvc patern

--- 005.python_workspace/student/02.students_function.py
+++ 005.python_workspace/student/02.students_function.py
@@ -1,141 +1,95 @@
-# 수강생 관리 시스템
-students = []
-sid = 1
-line = '='*6
-NOT_FOUND = -1
+#수강생 관리 시스템
+students=[] #global variable
 
+#수강생 등록 : list students에 id중복체크하고 존재하지 않는 경우 정보 저장하고 message 리턴
+def register(student) :
+    index = is_exist(student["id"])
+    if index < 0 : 
+        students.append(student)
+        return "{0}(이)가 등록되었습니다.".format(student["name"])
+    else :
+        return "이미 등록된 정보입니다." 
 
-# 수강생 등록 : list students에 정보 저장
-def register(name, id, major):
-    global sid, students
-
-    student = {'id':sid, 'name':name, 'age': int(age), 'major': major}
-    students.append(student)
-    sid += 1
-    print('{0}이 성공적으로 등록됐습니다.'.format(name))
-
-
-# 수강생 목록 : list students 목록 리턴
-def display_students():
-    global students
-
-    for std in students:
-        print('id: {0}, name: {1}, age: {2}, major: {3}'.format(std['id'],
-         std['name'], std['age'], std['major']))
-
-
-# update(), remove()에서 공통적으로 사용되는 id 입력 함수 
-def enter_id():
-    _sid = input('학생의 전공을 수정할 ID를 입력하세요 : ')
-        
-    while not _sid.isdigit():
-        print('숫자를 입력하세요')
-        _sid = input('ID를 입력하세요. : ')
-    
-    return int(_sid)
-
-
-# 수강생 수정 : id를 검색해서 전공정보 수정하고 message 리턴
-def update(std_index):
-    _major = input('변경할 {0}의 전공을 입력하세요. : '.format(students[std_index]['name']))
-    _name = students[std_index]['name']
-    students[std_index]['major'] = _major
-    print('{0}의 전공이 {1}으로 수정됐습니다.'.format(_name, _major))
-
- 
-# 수강생 삭제 : id를 검색해서 수강생 정보 삭제 message 리턴
-def remove(std_index):
-    global students
-
-    _name = students[std_index]['name']
-    del students[std_index]
-    print('{0}의 정보가 삭제됐습니다.'.format(_name))
     
 
-# 학생이 한명이라도 존재하는지 알아보는 함수
-def has_any_students():
-    global students
+#수강생 목록 : list students 목록 리턴
+def getAllStudents():
+    return students
 
-    if students: return True
-    return False
+#수강생 수정 : id를 검색해서 전공정보 수정하고 message 리턴
+def update(id, major):
+    index = is_exist(id)
+    if index > -1 :
+        students[index]["major"] = major 
+        return "{0}의 전공 정보가 수정되었습니다.".format(id)
+    else :
+        return "{0}의 정보가 존재하기 않습니다.".format(id)
 
-# 수강생 존재여부 : id 검색해서 students list의 index 값 리턴
-def has_student_id(std_id):
-    global students
+#수강생 삭제 :id를 검색해서 수강생 정보 삭제 message 리턴
+def remove(id) :
+    index = is_exist(id)
+    if index > -1 :
+        students.pop(index)
+        return "{0} 정보를 삭제했습니다.".format(id)
+    else :
+        return "{0} 정보가 존재하지 않습니다.".format(id)
 
-    for i, std in enumerate(students):
-        if std['id'] == std_id: return i
-    
-    return NOT_FOUND
+#수강생 존재여부 : id 검색해서 list students의 index 값 리턴
+def is_exist(id):
+    for index, student in enumerate(students):
+        if student["id"] == id :
+            return index
+    return -1
 
-# 메뉴 출력
-def display_menu():
-    print(line + ' Cloud MSA 반 수강생 관리 시스템 ' + line)
-    print('1. 수강생 정보 등록')
-    print('2. 수강생 목록 보기')
-    print('3. 수강생 정보 수정')
-    print('4. 수강생 정보 삭제')
-    print('0. 종료')
-    print()
+#menu display
+def menu_dispay():
+    print("====== Cloud MSA 반 수강생 관리 시스템 ======")
+    print("1.수강생 정보 등록")
+    print("2.수강생 목록 보기")
+    print("3.수강생 정보 수정")
+    print("4.수강생 정보 삭제")
+    print("0.종료")
 
+#message display
+def message_display(message):
+    print(message)
 
 while True:
-    display_menu()
+    menu_dispay()
 
-    menu = input('메뉴를 선택하세요 : ')
-
-    if menu == '1':
-        name = input('이름 : ')
-        age = input('나이 : ')
+    menu = input("메뉴를 선택하세요 : ")
+    if menu =="1" :
+        id = input("학번:")
+        name = input("이름 : ")
+        age = input("나이 : ")
+        while not age.isdecimal() :
+            print("나이는 숫자로 입력해 주세요.")
+            age=input("나이 : ")
+        major = input("전공 : ")
+        student ={"id":id, "name":name, "age":int(age), "major":major  }
         
-        while not age.isdigit(): 
-            print('나이는 숫자로 입력하세요.')
-            age = input('나이 : ')
-
-        major = input('전공 : ')
-        register(name, age, major)
-
-    elif menu == '2':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
-
-        display_students()
+        message_display(register(student))
+       
+    elif menu =="2" :
+        print("===== 수강생 목록 보기 =====")
+        print(students)
+    elif menu =="3" :
+        id = input("수정할 수강생 번호 : ")
+        major = input("수정할 전공 : ")
         
-    elif menu == '3':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
+        message_display( update(id, major) )
+       
+    elif menu=="4" :
+        id = input("삭제할 수강생 번호 : ")
         
-        _sid = enter_id()
-        std_index = has_student_id(_sid)
-
-        if std_index == NOT_FOUND:
-            print('존재하지 않는 ID입니다.')
-            continue
-
-        update(std_index)
-        
-
-    elif menu == '4':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
-
-        _sid = enter_id()
-        std_index = has_student_id(_sid)
-
-        if std_index == NOT_FOUND:
-            print('존재하지 않는 ID 입니다.')
-            continue
-
-        remove(std_index)
-        
-
-    elif menu == '0': 
-        print('수강생 관리 프로그램을 종료합니다.')
+        message_display( remove(id) )
+      
+    elif menu=="0" :
+        message_display("수강생 관리 프로그램을 종료합니다.")
         break
-
-    else: 
+    else :
         print()
-        print('1, 2, 3, 4, 0 번 중 선택하세요')
+        message_display("1,2,3,4,0 번 중 선택하세요" )
+#github : python_workshop repository생성  - 01.todos.py 작성해보기 
+#                                          {"todoNum":todoNum, "title":title}데이터 
+#                                          등록, 수정, 삭제, 일정목록, 전체삭제 기능
--- 005.python_workspace/student/03.students_function_io.py
+++ 005.python_workspace/student/03.students_function_io.py
@@ -1,178 +1,124 @@
-import os
+import os.path
 
-# 수강생 관리 시스템
-students = []
-sid = 1
-line = '='*6
-NOT_FOUND = -1
+#수강생 관리 시스템
+students=[] #global variable
 
+#수강생 등록 : list students에 id중복체크하고 존재하지 않는 경우 정보 저장하고 message 리턴
+def register(student) :
+    index = is_exist(student["id"])
+    if index < 0 : 
+        students.append(student)
+        return "{0}(이)가 등록되었습니다.".format(student["name"])
+    else :
+        return "이미 등록된 정보입니다." 
 
-# 수강생 등록 : list students에 정보 저장
-def register(name, id, major):
-    global sid, students
+#수강생 목록 : list students 목록 리턴
+def getAllStudents():
+    return students
 
-    student = {'id':sid, 'name':name, 'age': int(age), 'major': major}
-    students.append(student)
-    sid += 1
-    print('{0}이 성공적으로 등록됐습니다.'.format(name))
+#수강생 수정 : id를 검색해서 전공정보 수정하고 message 리턴
+def update(id, major):
+    index = is_exist(id)
+    if index > -1 :
+        students[index]["major"] = major 
+        return "{0}의 전공 정보가 수정되었습니다.".format(id)
+    else :
+        return "{0}의 정보가 존재하기 않습니다.".format(id)
 
+#수강생 삭제 :id를 검색해서 수강생 정보 삭제 message 리턴
+def remove(id) :
+    index = is_exist(id)
+    if index > -1 :
+        students.pop(index)
+        return "{0} 정보를 삭제했습니다.".format(id)
+    else :
+        return "{0} 정보가 존재하지 않습니다.".format(id)
 
-# 수강생 목록 : list students 목록 리턴
-def display_students():
-    global students
+#수강생 존재여부 : id 검색해서 list students의 index 값 리턴
+def is_exist(id):
+    for index, student in enumerate(students):
+        if student["id"] == id :
+            return index
+    return -1
 
-    for std in students:
-        print('id: {0}, name: {1}, age: {2}, major: {3}'.format(std['id'],
-         std['name'], std['age'], std['major']))
+#menu display
+def menu_dispay():
+    print("====== Cloud MSA 반 수강생 관리 시스템 ======")
+    print("1.수강생 정보 등록")
+    print("2.수강생 목록 보기")
+    print("3.수강생 정보 수정")
+    print("4.수강생 정보 삭제")
+    print("0.종료")
 
-
-# update(), remove()에서 공통적으로 사용되는 id 입력 함수 
-def enter_id():
-    _sid = input('학생의 전공을 수정할 ID를 입력하세요 : ')
-        
-    while not _sid.isdigit():
-        print('숫자를 입력하세요')
-        _sid = input('ID를 입력하세요. : ')
-    
-    return int(_sid)
-
-
-# 수강생 수정 : id를 검색해서 전공정보 수정하고 message 리턴
-def update(std_index):
-    _major = input('변경할 {0}의 전공을 입력하세요. : '.format(students[std_index]['name']))
-    _name = students[std_index]['name']
-    students[std_index]['major'] = _major
-    print('{0}의 전공이 {1}으로 수정됐습니다.'.format(_name, _major))
-
- 
-# 수강생 삭제 : id를 검색해서 수강생 정보 삭제 message 리턴
-def remove(std_index):
-    global students
-
-    _name = students[std_index]['name']
-    del students[std_index]
-    print('{0}의 정보가 삭제됐습니다.'.format(_name))
-    
-
-# 학생이 한명이라도 존재하는지 알아보는 함수
-def has_any_students():
-    global students
-
-    if students: return True
-    return False
-
-# 수강생 존재여부 : id 검색해서 students list의 index 값 리턴
-def has_student_id(std_id):
-    global students
-
-    for i, std in enumerate(students):
-        if std['id'] == std_id: return i
-    
-    return NOT_FOUND
-
-# 메뉴 출력
-def display_menu():
-    print(line + ' Cloud MSA 반 수강생 관리 시스템 ' + line)
-    print('1. 수강생 정보 등록')
-    print('2. 수강생 목록 보기')
-    print('3. 수강생 정보 수정')
-    print('4. 수강생 정보 삭제')
-    print('0. 종료')
-    print()
+#message display
+def message_display(message):
+    print(message)
 
 
-# 프로그램 종료시 students.dat 파일에 저장.
-def save_list_to_file():
-    global students
+#프로그램 종료시 list students "students.dat" 파일 저장
+def save_list() :
+    save_file = open("students.dat", "w")
+    for index, student in enumerate(students) :
+        save_file.write("{0}번째 | {1},{2},{3},{4}\n".format(index, student["id"],student["name"],student["age"],student["major"]))
 
-    with open('students.dat', 'w') as f:
-        for i, std in enumerate(students):
-            f.write('{0}번째 | {1},{2},{3},{4}\n'.format(i, std['id'],
-             std['name'], std['age'], std['major']))
+    save_file.close()
 
-    print('파일이 저장됐습니다.')
+#프로그램 시작시 "students.dat" 파일이 존재하고 정보가 있는 경우  list students초기화
+def init_data_load() :
+    fileExist = os.path.isfile("students.dat")
+    if fileExist :
+        read_file = open("students.dat", "r")
+        while True:
+            data = read_file.readline()
+            if len( data.split("|") )==2 :
+                student = data.split("|")[1].strip("\n").split(",")
+                students.append({"id":student[0].strip(), "name":student[1].strip(),"age":int(student[2].strip()),"major":student[3].strip()})
+            if not data : break
+        read_file.close()
 
-
-# 프로그램 시작시 student.dat 파일을 불러온다.
-def load_list_from_file():
-    global students, sid
-    has_file = os.path.isfile('students.dat')
-
-    if has_file:
-        with open('students.dat', 'r') as f:
-            while True:
-                data = f.readline()
-
-                if not data: break
-
-                if len(data.split('|')) == 2:
-                    std_info = data.split('|')[1].strip().rstrip().split(',')
-
-                    students.append({'id': int(std_info[0]), 'name': std_info[1],
-                     'age': int(std_info[2]), 'major': std_info[3]})
-            
-            sid = students[-1]['id'] + 1
-        
-
-load_list_from_file()
-
+init_data_load()
 while True:
-    display_menu()
+    menu_dispay()
 
-    menu = input('메뉴를 선택하세요 : ')
-
-    if menu == '1':
-        name = input('이름 : ')
-        age = input('나이 : ')
+    menu = input("메뉴를 선택하세요 : ")
+    if menu =="1" :
+        id = input("학번:")
+        name = input("이름 : ")
+        age = input("나이 : ")
+        while not age.isdecimal() :
+            print("나이는 숫자로 입력해 주세요.")
+            age=input("나이 : ")
+        major = input("전공 : ")
+        student ={"id":id, "name":name, "age":int(age), "major":major  }
         
-        while not age.isdigit(): 
-            print('나이는 숫자로 입력하세요.')
-            age = input('나이 : ')
-        major = input('전공 : ')
-        register(name, age, major)
-
-    elif menu == '2':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
-
-        display_students()
+        message_display(register(student))
+       
+    elif menu =="2" :
+        print("===== 수강생 목록 보기 =====")
+        print(students)
+    elif menu =="3" :
+        id = input("수정할 수강생 번호 : ")
+        major = input("수정할 전공 : ")
         
-    elif menu == '3':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
+        message_display( update(id, major) )
+       
+    elif menu=="4" :
+        id = input("삭제할 수강생 번호 : ")
         
-        _sid = enter_id()
-        std_index = has_student_id(_sid)
-
-        if std_index == NOT_FOUND:
-            print('존재하지 않는 ID입니다.')
-            continue
-
-        update(std_index)
-        
-
-    elif menu == '4':
-        if not has_any_students():
-            print('등록된 학생이 한명도 없습니다.')
-            continue
-
-        _sid = enter_id()
-        std_index = has_student_id(_sid)
-
-        if std_index == NOT_FOUND:
-            print('존재하지 않는 ID 입니다.')
-            continue
-
-        remove(std_index)
-        
-
-    elif menu == '0': 
-        save_list_to_file()
-        print('수강생 관리 프로그램을 종료합니다.')
+        message_display( remove(id) )
+      
+    elif menu=="0" :
+        message_display("수강생 관리 프로그램을 종료합니다.")
+        save_list()
         break
-
-    else: 
+    else :
         print()
-        print('1, 2, 3, 4, 0 번 중 선택하세요')
+        message_display("1,2,3,4,0 번 중 선택하세요" )
+
+
+#==============과제==================================================================================        
+#github : python_workshop repository  - 02.todos_fileio.py 작성해보기 
+#                                          {"todoNum":todoNum, "title":title}데이터 
+#                                          등록, 수정, 삭제, 일정목록, 전체삭제 기능, dispaly 모듈화
+#                                          종료시 todos.dat에 저장
+#                                          시작시 todos.dat 파일 읽어서 todos list에 초기화  
